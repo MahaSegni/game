d1e90302badd350414086b363d737b66d3e57cdb
narjessbencheikh <47827834+narjessbencheikh@users.noreply.github.com>
2019-04-09 13:11:40 -0700

    Add files via upload

---
+++ NARJESS/main.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<time.h>
+#include"SDL/SDL.h"
+#include"SDL/SDL_image.h"
+#include"SDL/SDL_mixer.h"
+#include"dep.h"
+#include"dep.c"
+
+int  main()
+{
+
+SDL_Surface*screen;
+SDL_Rect positionecran;
+SDL_Event event;
+
+
+
+enemy E;
+
+
+    SDL_Init(SDL_INIT_VIDEO);
+    
+
+    screen = SDL_SetVideoMode(640, 480, 32, SDL_HWSURFACE | SDL_DOUBLEBUF);
+    SDL_WM_SetCaption("Afficher Ennemie en Mvt Aleatoire Va et Vient sur x", NULL);
+ initialiser_enemy(&E);
+   
+
+deplacement_aleatoire(E,screen);
+
+  SDL_FreeSurface(E.image_enemy);
+    SDL_Quit();
+
+
+
+
+}
