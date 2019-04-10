c41d67702034c163561d9609f02b602623121eed
mahasg <47830677+mahasg@users.noreply.github.com>
2019-04-10 21:25:03 +0100

    menu

---
+++ MAHA/main1.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include <SDL/SDL_mixer.h>
+#include <SDL/SDL_ttf.h>
+#include "fonctions.c"
+int main()
+{
+menu();
+return 0 ;
+}
